07ea9673479e27ea0e60b165eba0cf750304a9d1
Tushar20-20Mahajan <tusharmahajan080@gmail.com>
2024-03-19 17:10:56 +0530

    changes done and new functuions like newRound , Update UI  and letterButtonPressed added

---
+++ ApplePie/Game.swift
@@ -0,0 +1,8 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by student on 19/03/24.
+//
+
+import Foundation
---
+++ Game.swift
@@ -0,0 +1,8 @@
+//
+//  Game.swift
+//  
+//
+//  Created by student on 19/03/24.
+//
+
+import Foundation
